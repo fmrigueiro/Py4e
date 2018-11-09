6a2a61490d08ab95eaa64fdc1f050e50ff24fc2b
Filipe <fmrigueiro@gmail.com>
2018-11-09 15:52:44 +0000

    New Lesson

--- Py4E_5.1.py
+++ Py4E_5.1.py
@@ -23,3 +23,21 @@ while True:
 	print(line)
 print("Yeah!!")
 
+
+for i in [1,2,3,4,5]:
+	print(i)
+print('blastoff')
+
+friends = ['ana', 'ze', 'pires']
+for friend in friends:
+	print('Hello', friend)
+print('thanks to all')
+
+longest_so_far = -1
+print('Before', longest_so_far)
+for the_num in [9,41,12,14,74,15,6]:
+	if the_num > longest_so_far:
+		longest_so_far = the_num
+	print(longest_so_far, the_num)
+print('After', longest_so_far)
+
