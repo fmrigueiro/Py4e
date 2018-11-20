65287da49cb5d4a59a7cfe815025917b0dd45b00
Filipe <fmrigueiro@gmail.com>
2018-11-20 16:52:55 +0000

    Strip white space
    Signed-off-by: Filipe <fmrigueiro@gmail.com>

--- Python Data Structures/Py4E_7.1.py
+++ Python Data Structures/Py4E_7.1.py
@@ -1,4 +1,28 @@
 # Python data Structures
 # Loading files
 
-xfile = open('mbox.txt')
+xfile = open('/Users/filipe/Documents/Python Learning/Py4e/Python Data Structures/mbox.txt')
+
+for cheese in xfile:
+	print(cheese)
+
+fhand = open('/Users/filipe/Documents/Python Learning/Py4e/Python Data Structures/mbox.txt')
+inp = fhand.read()
+# Length of the string
+print(len(inp))
+
+print(inp[:20])
+
+# Searching for Specific text in file
+
+for line in fhand:
+	if line.startswith('From: '):
+		print(line)
+
+# striping the new line (\n)
+# rstrip takes away the whitespace
+fhand = open('/Users/filipe/Documents/Python Learning/Py4e/Python Data Structures/mbox.txt')
+for line in fhand:
+	line = line.rstrip()
+	if line.startswith('From:'):
+		print(line)
