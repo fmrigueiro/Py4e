932c8e5ca7edf35e86e4b242eced665d9b31cee5
Filipe <fmrigueiro@gmail.com>
2018-12-31 15:35:36 +0000

    Tuples

--- Python Tuples/py4e_10_1.py
+++ Python Tuples/py4e_10_1.py
@@ -15,3 +15,33 @@ tups = d.items()
 print(tups)
 
 
+(0,1,2) < (5,1,2)
+
+
+d['zaquis'] = 22
+d['jjs1'] = 1
+
+sorted(d.items())
+
+for k,v in sorted(d.items()):
+	print(k, v)
+
+c = d
+tmp = list()
+for k, v in c.items():
+	tmp.append((v,k))
+
+print(tmp)
+tmp = sorted(tmp, reverse=True)
+print(tmp)
+
+
+c = {'a':10, 'b':5, 'c':22}
+
+print(sorted([(v,k) for k,v in c.items()]))
+
+c = tuple()
+
+x = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}
+y = x.items()
+
